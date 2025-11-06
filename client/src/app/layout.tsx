783dfd39f714565b019374365201e5e692cb4801
ErikaKK <491649804@qq.com>
2025-11-06 02:39:53 +0000

    edit config and docs

---
+++ client/src/app/layout.tsx
@@ -0,0 +1,15 @@
+import "@/styles/globals.css";
+
+import { ReactNode } from "react";
+
+import Providers from "./providers";
+
+export default function RootLayout({ children }: { children: ReactNode }) {
+  return (
+    <html lang="en">
+      <body>
+        <Providers>{children}</Providers>
+      </body>
+    </html>
+  );
+}
