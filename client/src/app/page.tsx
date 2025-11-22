d0ccef97cfa6aed88fde1c723421e877e4edf7be
Frasier <fsundra@gmail.com>
2025-11-22 20:45:20 +0800

    Adds exported figma css -- Changes font to Montserrat

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -22,7 +22,7 @@ export default function Home() {
   return (
     <main
       className={cn(
-        "flex min-h-screen flex-col items-center gap-4 p-24 font-sans",
+        "flex min-h-screen flex-col items-center gap-4 p-24",
         fontSans.variable,
       )}
     >
--- client/tailwind.config.ts
+++ client/tailwind.config.ts
@@ -20,7 +20,8 @@ const config = {
     },
     extend: {
       fontFamily: {
-        sans: ["var(--font-sans)", ...fontFamily.sans],
+        // sans: ["var(--font-sans)", ...fontFamily.sans],
+        montserrat: ["var(--font-montserrat)", "sans-serif"],
       },
       colors: {
         border: "hsl(var(--border))",
