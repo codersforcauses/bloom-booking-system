12f538111522c6085126597351b0f941234e0638
Yingtong Wu (Eden) <EdenWuyt@gmail.com>
2025-11-25 17:28:19 +0000

    Implement and test GET /api/bookings/search.

--- server/api/booking/urls.py
+++ server/api/booking/urls.py
@@ -1,10 +1,11 @@
 # from django.shortcuts import render
 from django.urls import path
 from .views import test
-from .views import BookingsListCreatView, BookingUpdateDeleteView
+from .views import BookingsListCreatView, BookingUpdateDeleteView, BookingSearchView
 
 urlpatterns = [
     path('', BookingsListCreatView.as_view()),
     path('<int:pk>/', BookingUpdateDeleteView.as_view()),
+    path('search/', BookingSearchView.as_view()),
     path('test/', test),
 ]
