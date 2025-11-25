44d6ef831a77462970e4362951f44201ef4fe3bc
Yingtong Wu (Eden) <EdenWuyt@gmail.com>
2025-11-25 17:20:04 +0000

    Implement and test PUT and DELETE methods. Update serializers and GET and POST methods accordingly.

--- server/api/booking/urls.py
+++ server/api/booking/urls.py
@@ -1,9 +1,10 @@
 # from django.shortcuts import render
 from django.urls import path
 from .views import test
-from .views import BookingsListCreatView
+from .views import BookingsListCreatView, BookingUpdateDeleteView
 
 urlpatterns = [
     path('', BookingsListCreatView.as_view()),
+    path('<int:pk>/', BookingUpdateDeleteView.as_view()),
     path('test/', test),
 ]
