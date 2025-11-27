687c296841b27f8ab514f5de0d2b0733164e8d37
Melissa Bauer <1999mbauer@gmail.com>
2025-11-27 06:29:14 +0000

    Inputed all changes from PR

--- client/src/components/ui/button.tsx
+++ client/src/components/ui/button.tsx
@@ -5,25 +5,22 @@ import * as React from "react";
 import { cn } from "@/lib/utils";
 
 const buttonVariants = cva(
-  "inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50",
+  "inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-normal ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50",
   {
     variants: {
       variant: {
         default: "bg-primary text-primary-foreground hover:bg-primary/90",
         destructive:
           "bg-destructive text-destructive-foreground hover:bg-destructive/90",
-        outline:
-          "border border-input bg-background hover:bg-accent hover:text-accent-foreground",
         secondary:
           "bg-secondary text-secondary-foreground hover:bg-secondary/80",
         ghost: "hover:bg-accent hover:text-accent-foreground",
-        link: 'relative text-primary hover:after:content-[""] hover:after:absolute hover:after:left-0 hover:after:-bottom-0.5 hover:after:h-0.5 hover:after:w-full hover:after:bg-primary hover:after:rounded hover:after:transition-all after:transition-all',
-        login: "text-10xl hover:bg-bloom-yellow",
-        outlineTest:
-          "border border-black hover:bg-accent hover:bg-bloom-blue-light",
-        confirm: "bg-bloom-blue hover:bg-accent hover:bg-bloom-blue-light",
-        warning: "bg-bloom-red text-white hover:bg-red-500",
-        bookings: "bg-bloom-orbit text-white hover:bg-bloom-orbit-light",
+        link: "text-primary underline-offset-8 hover:underline decoration-2",
+        login: "flex gap-2 items-centre hover:bg-bloom-yellow",
+        outline: "border border-black hover:bg-bloom-blue-light",
+        confirm: "bg-bloom-blue hover:bg-bloom-blue-light",
+        warning: "bg-bloom-red text-white hover:bg-bloom-red/80",
+        booking: "bg-bloom-orbit text-white hover:bg-bloom-orbit-light",
       },
       size: {
         default: "h-10 px-4 py-2",
