8b4eacf550415a7828736996149ccc495ef02115
Yingtong Wu (Eden) <EdenWuyt@gmail.com>
2025-11-28 10:20:25 +0000

    Remove check req.method != POST

--- client/src/app/api/verify-recaptcha/route.ts
+++ client/src/app/api/verify-recaptcha/route.ts
@@ -2,13 +2,6 @@ import axios from "axios";
 
 // RECAPTCHA_SECRET_KEY in .env (not syncronized in git) obtained from https://www.google.com/recaptcha/admin/create
 export async function POST(req: Request) {
-  if (req.method !== "POST") {
-    return new Response(
-      JSON.stringify({ message: "Only POST requests allowed" }),
-      { status: 405 },
-    );
-  }
-
   const data = await req.json();
   const { token } = data;
   const secretKey: string | undefined = process.env.RECAPTCHA_SECRET_KEY;
