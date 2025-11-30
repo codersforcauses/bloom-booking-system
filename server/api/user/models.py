370c11c54e445b81cd544a026e36683ad5e9bc94
ErikaKK <491649804@qq.com>
2025-11-30 16:34:23 +0000

    edit admin page

--- server/api/user/models.py
+++ server/api/user/models.py
@@ -1,7 +1,29 @@
 from django.db import models
-from django.contrib.auth.models import User as UserModel
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 
-class User(UserModel):
-    id = models.IntegerField(primary_key=True)
-    updated_at = models.DateTimeField()
+class UserProfile(models.Model):
+    user = models.OneToOneField(
+        User, on_delete=models.CASCADE, related_name="profile")
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"User {self.user.username}"
+
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    """Automatically create a UserProfile when a User is created"""
+    if created:
+        UserProfile.objects.create(user=instance)
+
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    """Automatically save the UserProfile when a User is saved"""
+    if hasattr(instance, 'profile'):
+        instance.profile.save()
+    else:
+        UserProfile.objects.create(user=instance)
