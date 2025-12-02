6a75198ebcbc1d8a57f9b6174f0678f48f712a6c
Fiona Wei <wyf25@hotmail.com>
2025-12-02 16:21:04 +0000

    Refactor authentication logic

--- server/api/user/serializers.py
+++ server/api/user/serializers.py
@@ -5,18 +5,11 @@ from rest_framework import exceptions
 
 class CustomTokenObtainPairSerializer(TokenObtainPairSerializer):
     def validate(self, attrs):
-        try:
-            user = authenticate(username=attrs['username'], password=attrs['password'])
-        except exceptions.ValidationError:
-            # If username authentication fails, try email authentication
-            try:
-                user = authenticate(email=attrs['username'], password=attrs['password'])
-            except exceptions.ValidationError:
-                # If email authentication also fails, or 'username' field is not provided
-                raise exceptions.AuthenticationFailed('Invalid credentials')
-
+        user = authenticate(username=attrs['username'], password=attrs['password'])
         if not user:
-            raise exceptions.AuthenticationFailed('Invalid credentials')
+            user = authenticate(email=attrs['username'], password=attrs['password'])
+            if not user:
+                raise exceptions.AuthenticationFailed('Invalid credentials')
 
         self.user = user
 
