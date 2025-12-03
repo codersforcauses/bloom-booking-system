a65b1b608cf731252ff8dece609d54a14f1a47b7
Yingtong Wu (Eden) <EdenWuyt@gmail.com>
2025-12-03 07:53:42 +0000

    Remove redundant api/bookings/test from urls.py. Update the README file.

--- server/api/booking/urls.py
+++ server/api/booking/urls.py
@@ -1,11 +1,9 @@
 # from django.shortcuts import render
 from django.urls import path
-from .views import test
 from .views import BookingsListCreatView, BookingListUpdateDeleteView, BookingSearchView
 
 urlpatterns = [
     path('', BookingsListCreatView.as_view()),
     path('<int:pk>/', BookingListUpdateDeleteView.as_view()),
     path('search/', BookingSearchView.as_view()),
-    path('test/', test),
 ]
