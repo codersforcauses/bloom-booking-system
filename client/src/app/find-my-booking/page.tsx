6458110560a55560716cf328df7e1736f8e7c3fe
rozza368 <45202958+rozza368@users.noreply.github.com>
2025-12-03 06:15:23 +0000

    Implement booking search API call

--- client/src/app/find-my-booking/page.tsx
+++ client/src/app/find-my-booking/page.tsx
@@ -5,22 +5,32 @@ import { useState } from "react";
 import { Button } from "@/components/ui/button";
 import InputField from "@/components/ui/input";
 import ReCAPTCHA_v2 from "@/components/ui/recaptchaV2";
+import api from "@/lib/api";
 
 export default function FindMyBookingPage() {
   const [verified, setVerified] = useState(false);
   const [email, setEmail] = useState("");
 
-  function callApi(formData: FormData) {
+  async function searchBookings(formData: FormData) {
     const email = formData.get("email");
-    alert(`Email: ${email}`);
-    // TODO
+    // TODO: sanitise input?
+    const apiUrl = `/api/bookings/search?visitor_email=${email}`;
+    await api({ url: apiUrl, method: "get" })
+      .then((response) => {
+        // TODO: handle success
+        console.log(response);
+      })
+      .catch((error) => {
+        // TODO: handle error
+        console.error("Error searching bookings:", error);
+      });
   }
 
   return (
     <div className="mx-auto max-w-lg space-y-6 bg-[hsl(var(--card))] p-6 text-[hsl(var(--card-foreground))]">
       <form
         className="flex flex-col flex-wrap items-center justify-center gap-8"
-        action={callApi}
+        action={searchBookings}
       >
         <h2 className="title mb-2">Enter your email</h2>
         <InputField
@@ -32,7 +42,7 @@ export default function FindMyBookingPage() {
           onChange={setEmail}
           placeholder="Enter your email"
         />
-        {/* captcha (need to wait for this?) */}
+        {/* TODO: captcha (need to wait for this?) */}
         {/* <ReCAPTCHA_v2 setVerified={setVerified}></ReCAPTCHA_v2> */}
         <Button variant={"default"}>Search</Button>
       </form>
