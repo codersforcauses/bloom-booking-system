fd429529f3947599f74be363f253c0bd2becbd6d
Yingtong Wu (Eden) <EdenWuyt@gmail.com>
2025-12-03 10:26:21 +0000

    Fix some typos.

--- server/api/booking/urls.py
+++ server/api/booking/urls.py
@@ -1,9 +1,9 @@
 # from django.shortcuts import render
 from django.urls import path
-from .views import BookingsListCreatView, BookingListUpdateDeleteView, BookingSearchView
+from .views import BookingsListCreateView, BookingListUpdateDeleteView, BookingSearchView
 
 urlpatterns = [
-    path('', BookingsListCreatView.as_view()),
+    path('', BookingsListCreateView.as_view()),
     path('<int:pk>/', BookingListUpdateDeleteView.as_view()),
     path('search/', BookingSearchView.as_view()),
 ]
