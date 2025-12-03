fa14a54803359a6cc4110000ebc4ef67518435a4
Jason Keo <163387808+jasonkeo@users.noreply.github.com>
2025-12-03 10:38:47 +0000

    Fixing Api responses to match requirement description

--- server/api/room/serializers.py
+++ server/api/room/serializers.py
@@ -1,8 +1,44 @@
 from rest_framework import serializers
-from .models import Room
+from .models import Room, Location, Capacity, Amenties
 
+class LocationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Location
+        fields = ["id", "name"]
 
+class CapacitySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Capacity
+        fields = ["id", "name"]
+
+class AmenitySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Amenties
+        fields = ["id", "name"]
+
+# 2 different serialiser because api requirements want more or less details depending on request type
 class RoomSerializer(serializers.ModelSerializer):
+    location = LocationSerializer(read_only=True)
+    capacity = CapacitySerializer(read_only=True)
+    amenities = AmenitySerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Room
+        fields = [
+            "id",
+            "name",
+            "img_url",
+            "location",
+            "capacity",
+            "amenities",
+        ]
+
+
+class RoomListSerializer(serializers.ModelSerializer):
+    location = LocationSerializer(read_only=True)
+    capacity = CapacitySerializer(read_only=True)
+    amenities = AmenitySerializer(many=True, read_only=True)
+
     class Meta:
         model = Room
         fields = "__all__"
