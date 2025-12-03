186505572aec4311608b43ba11d45995bd6b361b
rozza368 <45202958+rozza368@users.noreply.github.com>
2025-12-03 04:35:52 +0000

    Add basic email search page

---
+++ client/src/app/find-my-booking/page.tsx
@@ -0,0 +1,35 @@
+"use client";
+
+import { useState } from "react";
+
+import { Button } from "@/components/ui/button";
+import InputField from "@/components/ui/input";
+import ReCAPTCHA_v2 from "@/components/ui/recaptchaV2";
+
+export default function FindMyBookingPage() {
+  const [verified, setVerified] = useState(false);
+  const [email, setEmail] = useState("");
+
+  function callApi() {
+    return;
+  }
+
+  return (
+    <div className="mx-auto max-w-lg space-y-6 rounded-lg border bg-[hsl(var(--card))] p-6 text-[hsl(var(--card-foreground))] shadow-sm">
+      <h2 className="title mb-4">Enter your email</h2>
+      <form>
+        <InputField
+          kind="text"
+          label=""
+          name="email"
+          value={email}
+          onChange={setEmail}
+          placeholder="Email"
+        />
+        {/* captcha (need to wait for this?) */}
+        {/* <ReCAPTCHA_v2 setVerified={setVerified}></ReCAPTCHA_v2> */}
+        <Button variant={"confirm"}>Search</Button>
+      </form>
+    </div>
+  );
+}
