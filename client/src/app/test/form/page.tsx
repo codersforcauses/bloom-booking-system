f2d196f295551e00d12549904de899333c124387
Yingtong Wu (Eden) <EdenWuyt@gmail.com>
2025-12-05 11:08:26 +0000

    Add more tests about dropdown input, select input, radio buttons.

--- client/src/app/test/form/page.tsx
+++ client/src/app/test/form/page.tsx
@@ -5,6 +5,8 @@ import * as yup from "yup";
 import { Button } from "@/components/ui/button";
 import { ControlledField, Form } from "@/components/ui/form";
 import InputField from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group";
 
 const schema = yup.object({
   firstname: yup.string().required(),
@@ -20,6 +22,64 @@ const schema = yup.object({
     }),
 });
 
+const extendedSchema = yup.object({
+  firstname: yup.string().required(),
+  lastname: yup.string().required(),
+  middlename: yup.string(),
+  age: yup
+    .string()
+    .required()
+    .matches(/^\d+$/, "Age must be an non-negative integer")
+    .test("positive", "Age must be between 1 and 120", (value) => {
+      if (!value) return false;
+      return Number(value) >= 1 && Number(value) <= 120;
+    }),
+  frequency: yup
+    .string()
+    .required()
+    .oneOf(["daily", "weekly", "monthly"], "Invalid frequency"),
+  amenities: yup
+    .array()
+    .of(
+      yup
+        .string()
+        .oneOf(
+          [
+            "Audio",
+            "Video",
+            "White Board",
+            "HDMI",
+            "Projector",
+            "Speaker Phone",
+          ],
+          "Invalid amenities",
+        ),
+    )
+    .required(),
+  recurrenceDate: yup
+    .string()
+    .oneOf(["Monday", "Tuesday", "Wednesday"], "Invalid date")
+    .required(),
+});
+
+const FREQUENCIES = [
+  { label: "Daily", value: "daily" },
+  { label: "Weekly", value: "weekly" },
+  {
+    label: "Monthly",
+    value: "monthly",
+  },
+];
+
+const AMENITIES = [
+  "Audio",
+  "Video",
+  "White Board",
+  "HDMI",
+  "Projector",
+  "Speaker Phone",
+];
+
 export default function TestFormPage() {
   return (
     <>
@@ -27,7 +87,7 @@ export default function TestFormPage() {
       <div className="mx-auto w-full max-w-xl px-10 py-10">
         <p className="title text-center">Form 1 (default error handling)</p>
         <Form
-          schema={schema}
+          schema={extendedSchema}
           onSubmit={(data) => {
             alert("submitted data:\n" + JSON.stringify(data));
           }}
@@ -87,6 +147,62 @@ export default function TestFormPage() {
                 placeholder="Age"
                 required={true}
               />
+            )}
+          </ControlledField>
+
+          <ControlledField<string> name="frequency">
+            {({ value, onChange }) => (
+              <InputField
+                kind="select"
+                label="Frequency"
+                name="frequency"
+                options={FREQUENCIES}
+                value={value}
+                onChange={onChange}
+                placeholder="Select frequency"
+              />
+            )}
+          </ControlledField>
+
+          <ControlledField<string[]> name="amenities">
+            {({ value, onChange }) => (
+              <InputField
+                kind="badge"
+                label="Amenities"
+                name="amenities"
+                options={AMENITIES}
+                value={value}
+                onChange={onChange}
+                placeholder="Select Amenities"
+              />
+            )}
+          </ControlledField>
+
+          <ControlledField<string> name="recurrenceDate">
+            {({ value, onChange }) => (
+              <>
+                <p className="body-sm-bold">Recurrence Date</p>
+                <RadioGroup
+                  value={value}
+                  onValueChange={onChange}
+                  className="flex space-x-4"
+                >
+                  <div className="space-x-2">
+                    <RadioGroupItem value="Monday" id="Monday" />
+                    <Label htmlFor="Monday">Monday</Label>
+                  </div>
+
+                  <div className="space-x-2">
+                    <RadioGroupItem value="Tuesday" id="Tuesday" />
+                    <Label htmlFor="Tuesday">Tuesday</Label>
+                  </div>
+
+                  <div className="space-x-2">
+                    <RadioGroupItem value="Wednesday" id="Wednesday" />
+                    <Label htmlFor="Wednesday">Wednesday</Label>
+                  </div>
+                </RadioGroup>
+              </>
             )}
           </ControlledField>
 
