20523f391426d8929cfc6938a9f78c91ceeb5306
Yingtong Wu (Eden) <EdenWuyt@gmail.com>
2025-12-05 06:47:29 +0000

    Modify styling, allow get errors from FormProvider and allow className customization. Update tests and documentation.

--- client/src/app/test/form/page.tsx
+++ client/src/app/test/form/page.tsx
@@ -2,11 +2,14 @@
 import React from "react";
 import * as yup from "yup";
 
+import { Button } from "@/components/ui/button";
 import { ControlledField, Form } from "@/components/ui/form";
 import InputField from "@/components/ui/input";
 
 const schema = yup.object({
-  name: yup.string(), // optional field
+  firstname: yup.string().required(),
+  lastname: yup.string().required(),
+  middlename: yup.string(),
   age: yup
     .string()
     .required()
@@ -19,42 +22,271 @@ const schema = yup.object({
 
 export default function TestFormPage() {
   return (
-    <Form
-      schema={schema}
-      onSubmit={(data) => {
-        console.log("submitted", data);
-      }}
-      // onError={(errors) => console.log(Object.entries(errors))}
-    >
-      <ControlledField<string> name="name">
-        {({ value, onChange }) => (
-          <>
-            <InputField
-              kind="text"
-              label="Name"
-              name="name"
-              value={value}
-              onChange={onChange}
-              placeholder="Enter your name here..."
-            />
-            <p>This is an input</p>
-          </>
-        )}
-      </ControlledField>
+    <>
+      {/* Default error handling */}
+      <div className="mx-auto w-full max-w-xl px-10 py-10">
+        <p className="title text-center">Form 1 (default error handling)</p>
+        <Form
+          schema={schema}
+          onSubmit={(data) => {
+            alert("submitted data:\n" + JSON.stringify(data));
+          }}
+          // onError={(errors) => alert("errors:\n"+JSON.stringify(errors))}
+        >
+          <div className="flex flex-row gap-3">
+            <ControlledField<string> name="firstname">
+              {({ value, onChange }) => (
+                <InputField
+                  kind="text"
+                  label="First Name"
+                  name="firstname"
+                  value={value}
+                  onChange={onChange}
+                  placeholder="First Name"
+                  required={true}
+                />
+              )}
+            </ControlledField>
 
-      <ControlledField<string> name="age">
-        {({ value, onChange }) => (
-          <InputField
-            kind="number"
-            label="Age"
-            name="age"
-            value={value}
-            onChange={onChange}
-            placeholder="Enter your age here..."
-          />
-        )}
-      </ControlledField>
-      <input type="submit"></input>
-    </Form>
+            <ControlledField<string> name="middlename">
+              {({ value, onChange }) => (
+                <InputField
+                  kind="text"
+                  label="Middle Name"
+                  name="middlename"
+                  value={value}
+                  onChange={onChange}
+                  placeholder="Middle Name"
+                />
+              )}
+            </ControlledField>
+
+            <ControlledField<string> name="lastname">
+              {({ value, onChange }) => (
+                <InputField
+                  kind="text"
+                  label="Last Name"
+                  name="lastname"
+                  value={value}
+                  onChange={onChange}
+                  placeholder="Last Name"
+                  required={true}
+                />
+              )}
+            </ControlledField>
+          </div>
+
+          <ControlledField<string> name="age">
+            {({ value, onChange }) => (
+              <InputField
+                kind="number"
+                label="Age"
+                name="age"
+                value={value}
+                onChange={onChange}
+                placeholder="Age"
+                required={true}
+              />
+            )}
+          </ControlledField>
+
+          {/* Button */}
+          <div className="ml-auto space-x-3 space-y-2">
+            <Button
+              type="button"
+              variant="outline"
+              onClick={() => alert(JSON.stringify("Cancel button triggered"))}
+            >
+              Cancel
+            </Button>
+            <Button type="submit" variant="confirm">
+              Okay
+            </Button>
+          </div>
+        </Form>
+      </div>
+
+      {/* onError function */}
+      <div className="mx-auto w-full max-w-xl px-10 py-10">
+        <p className="title text-center">
+          Form 2 (customized onError function)
+        </p>
+        <Form
+          schema={schema}
+          onSubmit={(data) => {
+            alert("submitted data:\n" + JSON.stringify(data));
+          }}
+          onError={(errors) => alert("errors:\n" + JSON.stringify(errors))}
+          className="bg-background"
+        >
+          <div className="flex flex-row gap-3">
+            <ControlledField<string> name="firstname">
+              {({ value, onChange }) => (
+                <InputField
+                  kind="text"
+                  label="First Name"
+                  name="firstname"
+                  value={value}
+                  onChange={onChange}
+                  placeholder="First Name"
+                  required={true}
+                />
+              )}
+            </ControlledField>
+
+            <ControlledField<string> name="middlename">
+              {({ value, onChange }) => (
+                <InputField
+                  kind="text"
+                  label="Middle Name"
+                  name="middlename"
+                  value={value}
+                  onChange={onChange}
+                  placeholder="Middle Name"
+                />
+              )}
+            </ControlledField>
+
+            <ControlledField<string> name="lastname">
+              {({ value, onChange }) => (
+                <InputField
+                  kind="text"
+                  label="Last Name"
+                  name="lastname"
+                  value={value}
+                  onChange={onChange}
+                  placeholder="Last Name"
+                  required={true}
+                />
+              )}
+            </ControlledField>
+          </div>
+
+          <ControlledField<string> name="age">
+            {({ value, onChange }) => (
+              <InputField
+                kind="number"
+                label="Age"
+                name="age"
+                value={value}
+                onChange={onChange}
+                placeholder="Age"
+                required={true}
+              />
+            )}
+          </ControlledField>
+
+          {/* Button */}
+          <div className="ml-auto space-x-3 space-y-2">
+            <Button
+              type="button"
+              variant="outline"
+              onClick={() => alert(JSON.stringify("Cancel button triggered"))}
+            >
+              Cancel
+            </Button>
+            <Button type="submit" variant="confirm">
+              Okay
+            </Button>
+          </div>
+        </Form>
+      </div>
+
+      {/* get errors from FormProvider */}
+      <div className="mx-auto w-full max-w-xl px-10 py-10">
+        <p className="title text-center">
+          Form 3 (get errors from FormProvider)
+        </p>
+        <Form
+          schema={schema}
+          onSubmit={(data) => {
+            alert("submitted data:\n" + JSON.stringify(data));
+          }}
+          onError={(errors) => alert("errors:\n" + JSON.stringify(errors))}
+          className="bg-background"
+        >
+          {(methods) => (
+            <>
+              <div className="flex flex-row gap-3">
+                <ControlledField<string> name="firstname">
+                  {({ value, onChange }) => (
+                    <InputField
+                      kind="text"
+                      label="First Name"
+                      name="firstname"
+                      value={value}
+                      onChange={onChange}
+                      placeholder="First Name"
+                      required={true}
+                    />
+                  )}
+                </ControlledField>
+
+                <ControlledField<string> name="middlename">
+                  {({ value, onChange }) => (
+                    <InputField
+                      kind="text"
+                      label="Middle Name"
+                      name="middlename"
+                      value={value}
+                      onChange={onChange}
+                      placeholder="Middle Name"
+                    />
+                  )}
+                </ControlledField>
+
+                <ControlledField<string> name="lastname">
+                  {({ value, onChange }) => (
+                    <InputField
+                      kind="text"
+                      label="Last Name"
+                      name="lastname"
+                      value={value}
+                      onChange={onChange}
+                      placeholder="Last Name"
+                      required={true}
+                    />
+                  )}
+                </ControlledField>
+              </div>
+
+              <ControlledField<string> name="age">
+                {({ value, onChange }) => (
+                  <InputField
+                    kind="number"
+                    label="Age"
+                    name="age"
+                    value={value}
+                    onChange={onChange}
+                    placeholder="Age"
+                    required={true}
+                  />
+                )}
+              </ControlledField>
+
+              {/* Button */}
+              <div className="ml-auto space-x-3 space-y-2">
+                <Button
+                  type="button"
+                  variant="outline"
+                  onClick={() =>
+                    alert(JSON.stringify("Cancel button triggered"))
+                  }
+                >
+                  Cancel
+                </Button>
+                <Button
+                  type="submit"
+                  variant="confirm"
+                  disabled={Object.keys(methods.formState.errors).length > 0}
+                >
+                  Okay
+                </Button>
+              </div>
+            </>
+          )}
+        </Form>
+      </div>
+    </>
   );
 }
