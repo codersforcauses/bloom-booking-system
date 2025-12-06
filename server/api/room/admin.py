e139fc95664c8d515aa4ed44b19ba454fe63b3af
Jason Keo <163387808+jasonkeo@users.noreply.github.com>
2025-12-06 03:33:30 +0000

    added location & capicity to django admin page

--- server/api/room/admin.py
+++ server/api/room/admin.py
@@ -5,3 +5,11 @@ from .models import Room
 class RoomAdmin(admin.ModelAdmin):
     list_display = ("id", "name", "location_id", "capacity_id", "start_datetime", "end_datetime")
     search_fields = ("name",)
+
+class LocationAdmin(admin.ModelAdmin):
+    list_display = ("id", "name")
+    search_fields = ("name",)
+
+class CapacityAdmin(admin.ModelAdmin):
+    list_display = ("id", "name")
+    search_fields = ("name",)
