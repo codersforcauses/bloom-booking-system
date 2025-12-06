01769cdd798d426d55bf2fd32900c5322e5f29d5
Jason Keo <163387808+jasonkeo@users.noreply.github.com>
2025-12-06 07:07:15 +0000

    add status & change capicity to int

--- server/api/room/models.py
+++ server/api/room/models.py
@@ -13,19 +13,15 @@ class Amenties(models.Model):
     def __str__(self):
         return self.name
 
-class Capacity(models.Model):
-    id = models.AutoField(primary_key=True)
-    name = models.TextField(blank=False)
 
-    def __str__(self):
-        return str(self.name)
 class Room(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.TextField(blank=False)
     img = models.ImageField(upload_to='') # change to upload s3 later for deployment?
     location = models.ForeignKey(Location, on_delete=models.CASCADE, blank=False)
-    capacity = models.ForeignKey(Capacity, on_delete=models.CASCADE, blank=False)
+    capacity = models.IntegerField(blank=False)
     amenities = models.ManyToManyField(Amenties, blank=True)
+    status = models.BooleanField(default=True)
     start_datetime = models.DateTimeField(blank=False)
     end_datetime = models.DateTimeField(blank=False)
     recurrence_rule = models.TextField(blank=False)
@@ -35,5 +31,4 @@ class Room(models.Model):
     def __str__(self):
         return self.name
 # Create your models here.
-
 
