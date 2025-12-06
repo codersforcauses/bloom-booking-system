8b641e9c3982840e20e7b6fe6efb8fb01b9ebfc6
Yingtong Wu (Eden) <EdenWuyt@gmail.com>
2025-12-06 07:12:18 +0000

    Change manual booking filter for booking listing to use django-filter.

--- server/api/settings.py
+++ server/api/settings.py
@@ -58,6 +58,7 @@ INSTALLED_APPS = [
     "api.user",
     "api.room",
     "api.booking",
+    'django_filters',
 ]
 
 MIDDLEWARE = [
@@ -184,6 +185,11 @@ REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": ["rest_framework_simplejwt.authentication.JWTAuthentication"],
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
     "PAGE_SIZE": 10,
+    'DEFAULT_FILTER_BACKENDS': [
+        'django_filters.rest_framework.DjangoFilterBackend',
+        'rest_framework.filters.SearchFilter',
+        'rest_framework.filters.OrderingFilter',
+    ],
 }
 
 AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY_ID")
