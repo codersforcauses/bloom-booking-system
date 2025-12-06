7086bc594e61831687433cb34ee8a642d5695c9a
Jason Keo <163387808+jasonkeo@users.noreply.github.com>
2025-12-06 03:35:01 +0000

    fix add model to admin

--- server/api/room/admin.py
+++ server/api/room/admin.py
@@ -1,15 +1,17 @@
 from django.contrib import admin
-from .models import Room
+from .models import Room, Location, Capacity
 # Register your models here.
 @admin.register(Room)
 class RoomAdmin(admin.ModelAdmin):
     list_display = ("id", "name", "location_id", "capacity_id", "start_datetime", "end_datetime")
     search_fields = ("name",)
 
+@admin.register(Location)
 class LocationAdmin(admin.ModelAdmin):
     list_display = ("id", "name")
     search_fields = ("name",)
 
+@admin.register(Capacity)
 class CapacityAdmin(admin.ModelAdmin):
     list_display = ("id", "name")
     search_fields = ("name",)
