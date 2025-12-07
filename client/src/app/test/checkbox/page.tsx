3a22651862dcf96a8107dbb0021bbbe092d0fe66
Yingtong Wu (Eden) <EdenWuyt@gmail.com>
2025-12-07 18:40:50 +0000

    Add checkbox component. No test and documentation yet.

---
+++ client/src/app/test/checkbox/page.tsx
@@ -0,0 +1,14 @@
+"use client";
+import { useState } from "react";
+
+import { CheckboxGroup, CheckboxItem } from "@/components/ui/checkbox";
+
+const DATES = [
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+  "Sunday",
+];
