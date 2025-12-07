8fd0c331ed474266c15cff9eaf28e5998661a666
Jason Keo <163387808+jasonkeo@users.noreply.github.com>
2025-12-07 15:21:08 +0000

    rebase

--- server/api/room/views.py
+++ server/api/room/views.py
@@ -1,3 +1,45 @@
-from django.shortcuts import render
+from rest_framework import viewsets, permissions
+from rest_framework.response import Response
+from .models import Room
+from .serializers import RoomSerializer
 
-# Create your views here.
+class RoomViewSet(viewsets.ModelViewSet):
+    queryset = Room.objects.all()
+    serializer_class = RoomSerializer
+
+    def get_permissions(self):
+        if self.action in ["create", "update", "destroy"]:
+            return [permissions.IsAdminUser()]
+        return [permissions.AllowAny()]
+
+    def get_queryset(self):
+        qs = super().get_queryset()
+        params = self.request.query_params
+
+        if name := params.get("name"):
+            qs = qs.filter(name__icontains=name)
+
+        if loc := params.get("location_id"):
+            qs = qs.filter(location_id=loc)
+
+        if cap := params.get("capacity_id"):
+            qs = qs.filter(capacity_id=cap)
+
+        return qs
+
+    def update(self, request, *args, **kwargs):
+        instance = self.get_object()
+        serializer = self.get_serializer(instance, data=request.data, partial=True)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+
+        return Response({
+            "id": instance.id,
+            "name": instance.name,
+            "updated_at": instance.updated_at
+        })
+
+    def destroy(self, request, *args, **kwargs):
+        instance = self.get_object()
+        instance.delete()
+        return Response({"message": "Room deleted successfully"})
--- server/api/urls.py
+++ server/api/urls.py
@@ -27,10 +27,14 @@ from drf_spectacular.views import (
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/healthcheck/", include(("api.healthcheck.urls"))),
+<<<<<<< HEAD
     path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
     path("swagger/", SpectacularSwaggerView.as_view(url_name="schema"),
          name="swagger-ui"),
     path("redoc/", SpectacularRedocView.as_view(url_name="schema"),
          name="redoc"),
     path("api/users/", include(("api.user.urls"))),
+=======
+    path("api/rooms/", include(("api.room.urls"))),
+>>>>>>> 49af123 (add Tests & First Draft of Api)
 ]
