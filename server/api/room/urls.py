a7d522c328a75923ede9f1faa41a8304e0aa3977
Jason Keo <163387808+jasonkeo@users.noreply.github.com>
2025-12-07 15:21:08 +0000

    rebase

--- server/api/room/urls.py
+++ server/api/room/urls.py
@@ -1,6 +1,7 @@
-from django.urls import path
-from . import views
+from rest_framework.routers import DefaultRouter
+from .views import RoomViewSet
 
-app_name = "room"
-urlpatterns = [
-]
+router = DefaultRouter()
+router.register(r'', RoomViewSet, basename='rooms')
+
+urlpatterns = router.urls
