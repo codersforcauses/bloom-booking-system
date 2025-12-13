d2bceb0f3e2c09d23e32ec8e27f9d52135307043
Melissa Bauer <1999mbauer@gmail.com>
2025-12-13 01:08:36 +0000

    Add responsive navbar styling

--- client/src/components/ui/navbar.tsx
+++ client/src/components/ui/navbar.tsx
@@ -28,7 +28,7 @@ export default function Navbar() {
           <span
             className={cn(
               "block h-0.5 w-6 bg-black transition-all",
-              open && "translate-y-1 rotate-45",
+              open && "translate-y-1.5 rotate-45",
             )}
           />
           <span
@@ -40,7 +40,7 @@ export default function Navbar() {
           <span
             className={cn(
               "block h-0.5 w-6 bg-black transition-all",
-              open && "-translate-y-1 -rotate-45",
+              open && "-translate-y-1.5 -rotate-45",
             )}
           />
         </button>
@@ -53,27 +53,29 @@ export default function Navbar() {
           <Link href="/bookings" className="hover:text-primary">
             <Button variant="link">Find my booking</Button>
           </Link>
-          <Link href="/about" className="hover:text-primary">
+          <Link href="/admin" className="hover:text-primary">
             <Button variant="login">
-            <HiOutlineUser className="h-5 w-5" />
-              Admin login</Button>
+              <HiOutlineUser className="h-5 w-5" />
+              Admin login
+            </Button>
           </Link>
         </div>
       </div>
 
       {/* mobile hamburger */}
       {open && (
-        <div className="flex flex-col space-y-2 px-4 pb-4 md:hidden">
+        <div className="m-6 mt-6 flex flex-col items-end space-y-2 px-8 pb-4 md:hidden">
           <Link href="/" className="hover:text-primary">
             <Button variant="outline">Book room</Button>
           </Link>
           <Link href="/bookings" className="hover:text-primary">
             <Button variant="link">Find my booking</Button>
           </Link>
-          <Link href="/about" className="hover:text-primary">
+          <Link href="/admin" className="hover:text-primary">
             <Button variant="login">
-            <HiOutlineUser className="h-5 w-5" />
-              Admin login</Button>
+              <HiOutlineUser className="h-5 w-5" />
+              Admin login
+            </Button>
           </Link>
         </div>
       )}
