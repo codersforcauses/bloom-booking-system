25720ee65dda6eff02cc1bd634c0020f5dd285f8
ErikaKK <491649804@qq.com>
2025-12-24 09:36:14 +0000

    be api uplift

--- server/api/user/models.py
+++ server/api/user/models.py
@@ -1,20 +1,12 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.contrib.auth.models import UserManager
-from django.core.validators import validate_email
-from django.core.exceptions import ValidationError
 
 
 class CustomUserManager(UserManager):
     def create_user(self, username, email=None, password=None, **extra_fields):
         if not email:
             raise ValueError('Email is required')
-
-        # Validate email format
-        try:
-            validate_email(email)
-        except ValidationError:
-            raise ValueError('Invalid email format')
 
         return super().create_user(username, email, password, **extra_fields)
 
