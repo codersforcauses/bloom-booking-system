f3822375b6e70b20cd1e80fba2ba78fc848645e6
Yingtong Wu (Eden) <EdenWuyt@gmail.com>
2026-01-06 14:52:28 +0000

    Rebase on the latest origin/main and update input import.

--- client/src/app/test/form/page.tsx
+++ client/src/app/test/form/page.tsx
@@ -4,6 +4,7 @@ import React from "react";
 import { useForm } from "react-hook-form";
 import * as z from "zod";
 
+import InputField from "@/components/input";
 import { Button } from "@/components/ui/button";
 import {
   Form,
@@ -13,7 +14,6 @@ import {
   FormLabel,
   FormMessage,
 } from "@/components/ui/form";
-import InputField from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group";
 
