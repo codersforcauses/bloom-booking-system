44a2dbc928d6ad9bcd677c05ca0f703b56ae40fd
Yingtong Wu (Eden) <edenwuyt@gmail.com>
2026-01-17 10:10:09 +0000

    Add validation to check if API_SECRET_KEY exists to increase tracebility.

--- client/src/app/api/check-auth/route.ts
+++ client/src/app/api/check-auth/route.ts
@@ -3,13 +3,22 @@ import { NextResponse } from "next/server";
 
 export async function POST(req: Request) {
   const { accessToken } = await req.json();
-
+  const secretKey = process.env.API_SECRET_KEY;
+  if (!secretKey) {
+    throw new Error("API_SECRET_KEY is not set in environment variables");
+  }
   try {
     // Verify the JWT token
-    const secret = new TextEncoder().encode(process.env.API_SECRET_KEY);
+    const secret = new TextEncoder().encode(secretKey);
     await jwtVerify(accessToken, secret);
     return NextResponse.json({ valid: true });
   } catch (error) {
+    if (
+      error instanceof Error &&
+      error.message === "API_SECRET_KEY is not set in environment variables"
+    ) {
+      throw error;
+    }
     return NextResponse.json({ valid: false }, { status: 401 });
   }
 }
