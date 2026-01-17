1f029f6d867b305fc8ecfd9eab2b915f6d1c772b
Jason Keo <163387808+jasonkeo@users.noreply.github.com>
2026-01-17 04:49:34 +0000

    added cards with api

---
+++ client/src/app/meeting-room/filterbutton.tsx
@@ -0,0 +1,24 @@
+import { Button } from "@/components/ui/button";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+
+import FilterDropDown from "./filterdropdown";
+
+export default function FilterPopover() {
+  return (
+    <Popover>
+      <PopoverTrigger asChild>
+        <Button variant="outline" className="bg-white">
+          Filter
+        </Button>
+      </PopoverTrigger>
+
+      <PopoverContent align="end" sideOffset={8} className="w-auto p-0">
+        <FilterDropDown />
+      </PopoverContent>
+    </Popover>
+  );
+}
---
+++ client/src/app/meeting-room/page.tsx
@@ -0,0 +1,97 @@
+"use client";
+
+import { useEffect, useState } from "react";
+
+import Breadcrumb from "@/components/breadcrumb";
+import InputField from "@/components/input";
+import { AdminRoomCard } from "@/components/room-card";
+import { Button } from "@/components/ui/button";
+import api from "@/lib/api";
+import { Room , roomsMock } from "@/types/card";
+
+import FilterPopOver from "./filterbutton";
+
+export default function RoomsPage() {
+  const [rooms, setRooms] = useState<Room[]>([]);
+
+  async function fetchRooms() {
+    const apiUrl = "/rooms/";
+    await api({ url: apiUrl, method: "get" })
+      .then((response) => {
+        const mappedRooms = mapRooms(response.data.results);
+
+        setRooms(mappedRooms);
+        console.log(response.data.results);
+      })
+      .catch((error) => {
+        console.error("Error fetching rooms:", error);
+      });
+  }
+
+  // Bandaid fix need to change component paramters to match api response
+  function mapRooms(data: any[]) {
+    return data.map((r: any) => ({
+      title: r.name || "Untitled Room",
+      image: r.img || "/rooms/default.jpg",
+      location: r.location?.name || "Unknown",
+      seats: r.capacity ?? 0,
+      amenities: mapRoomNames(r.amenities) || [],
+      available: r.is_active ?? false,
+      bookings: 0,
+      removed: false,
+      id: r.id,
+    }));
+  }
+  function mapRoomNames(data: any[]): string[] {
+    return data.map((r: any) => r.name || "Untitled Room");
+  }
+
+  useEffect(() => {
+    fetchRooms();
+  }, []);
+
+  return (
+    <div className="">
+      <div className="bg-red-500"> Navbar here </div>
+      <div className="mx-auto px-2 py-2">
+        <Breadcrumb
+          items={[{ label: "Home", href: "/" }, { label: "Meeting Rooms" }]}
+        />
+      </div>
+      {/* change this to style colour */}
+      <div className="mx-auto my-auto min-h-screen bg-[#F9F9F9] px-10 py-5">
+        <div className="subtitle m-2 mx-auto mb-5 flex h-full py-2">
+          {" "}
+          Meeting Room
+          <div className="ml-auto flex items-center gap-2 whitespace-nowrap">
+            <InputField
+              className="w-[30rem]"
+              kind="text"
+              label=""
+              name="search"
+              // value={}
+              // onChange={}
+              placeholder="Search here"
+            />
+            <FilterPopOver />
+            <a href="/meeting-room/add">
+              <Button variant="confirm">Add Room</Button>
+            </a>
+          </div>
+        </div>
+
+        <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+          {rooms.map((room) => (
+            <AdminRoomCard
+              key={room.id}
+              room={room}
+              onView={() => alert("View")} // push to room details page maybe like string + id
+              onEdit={() => alert("Edit")}
+              onRemove={() => alert("Remove")}
+            />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+}
