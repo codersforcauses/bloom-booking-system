b920e77b6827edc040114820427bcee9cb77a564
ErikaKK <491649804@qq.com>
2026-01-31 05:14:48 +0000

    fix prettier

--- client/src/app/(admin)/meeting-room/page.tsx
+++ client/src/app/(admin)/meeting-room/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useEffect,useMemo, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 
 import { AdminRoomCard } from "@/components/room-card";
 import { Button } from "@/components/ui/button";
