4078d7f1a07ca2a6510361df64a1b5053afb0f7d
ErikaKK <491649804@qq.com>
2026-01-31 05:14:48 +0000

    fix prettier

--- client/src/app/(admin)/meeting-room/room-context.tsx
+++ client/src/app/(admin)/meeting-room/room-context.tsx
@@ -1,6 +1,6 @@
 import { createContext } from "react";
 
-import { AmenityResponse,LocationResponse } from "@/lib/api-types";
+import { AmenityResponse, LocationResponse } from "@/lib/api-types";
 import { Room } from "@/types/card";
 
 interface RoomContextType {
