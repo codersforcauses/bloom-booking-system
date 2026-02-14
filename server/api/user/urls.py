057db9d1552ef80acca7e504eaef57eeca6bc60d
Yuxin <wynonnas@outlook.com>
2026-02-14 01:51:59 +0000

    move auth validation from Next.js API route to Django backend

--- server/api/user/urls.py
+++ server/api/user/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
 from rest_framework_simplejwt.views import TokenRefreshView
-from .views import CustomTokenObtainPairView
+from .views import CustomTokenObtainPairView, check_auth, ping
 
 app_name = "user"
+
 urlpatterns = [
+    path('ping/', ping, name='ping'),
     path('login/', CustomTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('check-auth/', check_auth, name='check_auth'),
 ]
