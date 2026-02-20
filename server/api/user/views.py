49b2d9a82cd6f0a61f669250790b7af30fa8c628
ErikaKK <491649804@qq.com>
2026-02-20 17:17:03 +0000

    edit a bit

--- server/api/user/views.py
+++ server/api/user/views.py
@@ -1,11 +1,9 @@
-from django.contrib.auth import get_user_model
 from django.http import HttpResponse
 
-from rest_framework import serializers, status
+from rest_framework import status
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
-
 from rest_framework_simplejwt.views import TokenObtainPairView
 
 from .serializers import CustomTokenObtainPairSerializer
@@ -25,22 +23,13 @@ class CustomTokenObtainPairView(TokenObtainPairView):
     serializer_class = CustomTokenObtainPairSerializer
 
 
-# check-auth response
-class UserSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = get_user_model()
-        fields = ["id", "username", "email"]
-
-
 # Auth check endpoint
 @api_view(["GET"])
 @permission_classes([IsAuthenticated])
 def check_auth(request):
-    serializer = UserSerializer(request.user)
     return Response(
         {
             "valid": True,
-            "user": serializer.data,
         },
         status=status.HTTP_200_OK,
     )
