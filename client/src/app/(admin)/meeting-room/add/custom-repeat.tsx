cc4f83063aa1ac27df95e671560aebb5257db57e
ErikaKK <491649804@qq.com>
2026-03-28 06:58:46 +0000

    add disable date for date pickerr

--- client/src/app/(admin)/meeting-room/add/custom-repeat.tsx
+++ client/src/app/(admin)/meeting-room/add/custom-repeat.tsx
@@ -178,6 +178,7 @@ export default function CustomRepeatModal({
                     name="endDate"
                     label=""
                     value={endDate}
+                    disabledDates={(date) => date < new Date()}
                     onChange={setEndDate}
                     className="mb-0"
                   />
