2d9126954042d1680a4294922ffb6c38748f88fe
ErikaKK <491649804@qq.com>
2026-03-29 01:50:18 +0000

    update setting

--- server/api/settings.py
+++ server/api/settings.py
@@ -84,6 +84,9 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
     FRONTEND_URL,
+]
+
+CORS_ALLOWED_ORIGIN_REGEXES = [
     r"^https://bloom-booking-system.*\.vercel\.app$",
 ]
 
